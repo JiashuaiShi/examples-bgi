f428caa257ae1d6d7bf2af8d53876826802e9dac
GENOMICS\shijiashuai <shijiashuai@genomics.cn>
2022-06-22 10:17:00 +0800

    update

--- test/test-tmp/main.cpp
+++ test/test-tmp/main.cpp
@@ -1,21 +1,9 @@
-#include <string>
-#include <fstream>
 #include <iostream>
-
 using namespace std;
-
-int main() {
-    setvbuf(stdout, NULL, _IONBF, 0);
-
-    ifstream inFile("/mnt/d/lines.txt");
-    string line;
-
-    // 按行分割
-//#pragma omp parallel for
-    auto c = getline(inFile, line);
-    while (c) {
-        cout << line << endl;
-    }
-
+int main()
+{
+    int *a = new int[10];
+    a[11] = 0;
+    cout << a[11]<< endl;
     return 0;
 }
